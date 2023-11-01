19927e51313cac303f03e5a87d5727d9e60bca4b
Álvaro García Pizarro <81751965+alvarogarciapiz@users.noreply.github.com>
2023-11-01 16:07:28 +0100

    Addes Solutions Architech cooming soon

--- src/components/SolutionsArchitect.jsx
+++ src/components/SolutionsArchitect.jsx
@@ -2,17 +2,10 @@ import React from "react";
 
 function SolutionsArchitect() {
   return (
-    <>
-    <p>Página SolutionsArchitect Exam</p>
-    <p>Página SolutionsArchitect Exam</p>
-    <p>Página SolutionsArchitect Exam</p>
-    <p>Página SolutionsArchitect Exam</p>
-    <p>Página SolutionsArchitect Exam</p>
-    <p>Página SolutionsArchitect Exam</p>
-    <p>Página SolutionsArchitect Exam</p>
-    <p>Página SolutionsArchitect Exam</p>
-    <p>Página SolutionsArchitect Exam</p>
-    </>
+    <div style={{display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', height: '100vh'}}>
+      <h1 style={{fontSize: '2em', margin: '1 auto', maxWidth: '90%', textAlign: 'center'}}>Coming Soon</h1>
+      <p>SolutionsArchitect Exam</p>
+    </div>
   );
 }
 
