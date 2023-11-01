0e8269918891731718be40e413843b23c92c7bf1
Álvaro García Pizarro <81751965+alvarogarciapiz@users.noreply.github.com>
2023-11-01 18:08:43 +0100

    Fixed ghpages routing

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './assets/styles/app.css'
-import { BrowserRouter as Router } from 'react-router-dom';
+import { HashRouter as Router } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
