a15b82126b7ef8ae5ca136170c0d044cd5899fc3
Álvaro García Pizarro <81751965+alvarogarciapiz@users.noreply.github.com>
2023-11-01 18:21:11 +0100

    Fixed home main button

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,15 +1,18 @@
 import React from "react";
 import '../assets/styles/Home.css';
+import { Link } from 'react-router-dom';
 
 function Home() {
   return (
     <div className="home">
       <div className="home__left">
-      <h1 className="home__title">AWS<br/>Certify<br/><span className="orange">Hub</span></h1>
+        <h1 className="home__title">AWS<br />Certify<br /><span className="orange">Hub</span></h1>
       </div>
       <div className="home__right">
         <p className="home__text">Your all-in-one resource for AWS certification success, providing the knowledge and support you need to unlock AWS excellence and making AWS certifications accessible to all</p>
-        <button className="home__button">Start Learning</button>
+        <Link to="/cloudpractitioner">
+          <button className="home__button">Start Learning</button>
+        </Link>
       </div>
     </div>
   );
