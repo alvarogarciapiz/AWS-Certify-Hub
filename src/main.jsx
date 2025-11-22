725d656814450443d7f1fd3e929129f91fc02117
alvarogarciapiz <81751965+alvarogarciapiz@users.noreply.github.com>
2025-11-22 16:53:52 +0100

    fix(main): corregir la importación del archivo de estilos para mantener la consistencia

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './assets/styles/app.css'
+import './assets/styles/App.css'
 import { HashRouter as Router } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
